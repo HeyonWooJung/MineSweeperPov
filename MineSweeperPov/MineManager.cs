ce6ff7ba271d9a89f0bde687916aae80f6d580a7
정현우 <slander.jung@gmail.com>
2024-12-23 11:41:16 +0900

    [Feat]맵 생성 기능 추가
    지뢰 구조체, 맵 생성 기능과 테스트용 맵 출력 추가

--- MineSweeperPov/MineManager.cs
+++ MineSweeperPov/MineManager.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace MineSweeperPov
 {
-    internal class MineManager
+    struct Mine
     {
+        int _nearMines;
+        bool _isMine;
+        bool _isExplored;
+
+        public int NearMines
+        {
+            get { return _nearMines; }
+            set { _nearMines = value; }
+        }
+
+        public bool IsMine
+        {
+            get { return _isMine; }
+            set { _isMine = value; }
+        }
+
+        public bool IsExplored
+        {
+            get { return _isExplored; }
+            set { _isExplored = value; }
+        }
+
+    }
+
+    class MineManager
+    {
+        Mine[,] _map;
+
+        //맵 생성
+        public void MakeMap(int size, int mines, Random random)
+        {
+            _map = new Mine[size, size];
+            
+            for (int i = 0; i < mines; i++)
+            {
+                _map[random.Next(0, size + 1), random.Next(0, size + 1)].IsMine = true;
+            }
+        }
+
+        //맵 출력(테스트용)
+        public void PrintMap()
+        {
+            for(int i = 0; i < _map.GetLength(1);i++)
+            {
+                for(int j=0;  j < _map.GetLength(1); j++)
+                {
+                    Console.Write($"{_map[i,j].NearMines} {_map[i, j].IsMine} {_map[i, j].IsExplored} | ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
